0d1bb47afde108d2fac500d6b4ba801e9564e2d9
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-10-30 11:08:42 +0800

    access env variable added in serverless

--- src/storage/config.js
+++ src/storage/config.js
@@ -5,28 +5,28 @@ const config = () =>
     ? {
         s3: {
           s3ForcePathStyle: true,
-          accessKeyId: process.env.stg_IAM_KEY_ID,
-          secretAccessKey: process.env.stg_IAM_KEY_SECRET,
+          accessKeyId: process.env.IAM_KEY_ID,
+          secretAccessKey: process.env.IAM_KEY_SECRET,
           region: "us-west-2",
           endpoint: new AWS.Endpoint("http://localhost:8000")
         },
-        bucketName: process.env.stg_BUCKET_NAME,
+        bucketName: process.env.BUCKET_NAME,
         network:
-          process.env.stg_NETWORK === "undefined"
+          process.env.NETWORK === "undefined"
             ? "ropsten"
-            : process.env.stg_NETWORK
+            : process.env.NETWORK
       }
     : {
         s3: {
-          accessKeyId: process.env.prd_IAM_KEY_ID,
-          secretAccessKey: process.env.prd_IAM_KEY_SECRET,
-          region: process.env.prd_SES_REGION || "us-west-2"
+          accessKeyId: process.env.IAM_KEY_ID,
+          secretAccessKey: process.envIAM_KEY_SECRET,
+          region: process.env.SES_REGION || "us-west-2"
         },
-        bucketName: process.env.prd_BUCKET_NAME,
+        bucketName: process.env.BUCKET_NAME,
         network:
-          process.env.prd_NETWORK === "undefined"
+          process.env.NETWORK === "undefined"
             ? "homestead"
-            : process.env.prd_NETWORK
+            : process.env.NETWORK
       };
 
 module.exports = config();
