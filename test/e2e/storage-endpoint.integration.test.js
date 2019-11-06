233db29a896f325363a7ef21ea9c1f51f565d331
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-11-06 16:51:40 +0800

    updated test

--- test/e2e/storage-endpoint.integration.test.js
+++ test/e2e/storage-endpoint.integration.test.js
@@ -1,12 +1,57 @@
 const supertest = require("supertest");
-const { put } = require("../../src/storage/s3");
+const { put, remove } = require("../../src/storage/s3");
 const config = require("../../src/storage/config");
 const ropstenDocument = require("../fixtures/certificate.json");
 
 const API_ENDPOINT = "https://api-ropsten.opencerts.io";
 const request = supertest(API_ENDPOINT);
 
+const uuidV4Regex = new RegExp(
+  /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+);
+
+const thatIsUploadResponse = {
+  id: expect.stringMatching(uuidV4Regex),
+  key: expect.any(String),
+  type: expect.stringMatching("OPEN-ATTESTATION-TYPE-1")
+};
+
 describe("storage endpoint test", () => {
+  it("should create document when no placeholder object is there", async done => {
+    await request
+      .get("/storage/create")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .send({
+        document: ropstenDocument
+      })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect(async (err, res) => {
+        if (err) return done(err);
+        expect(res.body).toEqual(thatIsUploadResponse);
+        return remove({ Bucket: config.bucketName, Key: res.body.id });
+      });
+  }, 5000);
+
+  it("should create document when placeholder object is there", async done => {
+    await request
+      .get("/storage/create")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .send({
+        document: ropstenDocument,
+        id: "123"
+      })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect(async (err, res) => {
+        if (err) return done(err);
+        expect(res.id).toEqual("123");
+        return remove({ Bucket: config.bucketName, Key: "123" });
+      });
+  }, 5000);
+
   it("should throw error forbidden when directly access the document", async () => {
     const document = { foo: "bar" };
     const params = {
@@ -16,17 +61,19 @@ describe("storage endpoint test", () => {
     };
     const uploaded = await put(params);
 
-    await request
-      .get(`${uploaded.Location}`)
+    await supertest(`${uploaded.Location}`)
+      .get("/")
       .set("Content-Type", "application/json")
       .expect("Content-Type", /json/)
       .expect(403);
+
+    await remove(params);
   }, 5000);
 
   it("should create a placeholder object", async () => {
     let queueId = "";
     await request
-      .post("/storage/queue")
+      .get("/storage/queue")
       .set("Content-Type", "application/json")
       .set("Accept", "application/json")
       .send({
@@ -43,15 +90,20 @@ describe("storage endpoint test", () => {
       });
 
     await request
-      .get(`/storage/${queueId}`)
+      .get(`/storage/get/${queueId}`)
       .set("Content-Type", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200);
+      .expect(400);
 
-    await request
-      .get(`https://${config.bucketName}.s3.amazonaws.com/${queueId}`)
+    await supertest(`https://${config.bucketName}.s3.amazonaws.com`)
+      .get(`/${queueId}`)
       .set("Content-Type", "application/json")
-      .expect("Content-Type", /json/)
       .expect(403);
+
+    const params = {
+      Bucket: config.bucketName,
+      Key: queueId
+    };
+
+    await remove(params);
   }, 5000);
 });
