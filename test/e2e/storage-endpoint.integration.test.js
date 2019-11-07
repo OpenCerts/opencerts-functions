d10f3ad920fffebbefc19e4a66fbbac318d12028
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-11-07 11:45:57 +0800

    modify PR changes to remove created objects

--- test/e2e/storage-endpoint.integration.test.js
+++ test/e2e/storage-endpoint.integration.test.js
@@ -16,8 +16,16 @@ const thatIsUploadResponse = {
   type: expect.stringMatching("OPEN-ATTESTATION-TYPE-1")
 };
 
+const thatIsRetrievedDocument = {
+  document: expect.objectContaining({
+    cipherText: expect.any(String),
+    iv: expect.any(String),
+    tag: expect.any(String)
+  })
+};
+
 describe("storage endpoint test", () => {
-  it("should create document when no placeholder object is there", async done => {
+  it("should create document when no placeholder object is there", async () => {
     await request
       .get("/storage/create")
       .set("Content-Type", "application/json")
@@ -27,14 +35,13 @@ describe("storage endpoint test", () => {
       })
       .expect("Content-Type", /json/)
       .expect(200)
-      .expect(async (err, res) => {
-        if (err) return done(err);
+      .expect(async res => {
+        await remove({ Bucket: config.bucketName, Key: res.body.id });
         expect(res.body).toEqual(thatIsUploadResponse);
-        return remove({ Bucket: config.bucketName, Key: res.body.id });
       });
   }, 5000);
 
-  it("should create document when placeholder object is there", async done => {
+  it("should create document when placeholder object is there", async () => {
     await request
       .get("/storage/create")
       .set("Content-Type", "application/json")
@@ -45,65 +52,101 @@ describe("storage endpoint test", () => {
       })
       .expect("Content-Type", /json/)
       .expect(200)
-      .expect(async (err, res) => {
-        if (err) return done(err);
-        expect(res.id).toEqual("123");
-        return remove({ Bucket: config.bucketName, Key: "123" });
+      .expect(async res => {
+        await remove({ Bucket: config.bucketName, Key: "123" });
+        expect(res.body.id).toEqual("123");
       });
+  }, 5000);
+
+  it("should retrieve the document created", async () => {
+    try {
+      await request
+        .get("/storage/create")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          document: ropstenDocument,
+          id: "123"
+        })
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      await request
+        .get("/storage/get/123")
+        .set("Content-Type", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect(async res => {
+          await remove({ Bucket: config.bucketName, Key: "123" });
+          expect(res.body).toEqual(thatIsRetrievedDocument);
+        });
+    } catch (e) {
+      await remove({ Bucket: config.bucketName, Key: "123" });
+      throw e;
+    }
   }, 5000);
 
   it("should throw error forbidden when directly access the document", async () => {
     const document = { foo: "bar" };
     const params = {
       Bucket: config.bucketName,
-      Key: "abc",
+      Key: "123",
       Body: JSON.stringify({ document })
     };
-    const uploaded = await put(params);
 
-    await supertest(`${uploaded.Location}`)
-      .get("/")
-      .set("Content-Type", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(403);
+    try {
+      const uploaded = await put(params);
+      await supertest(`${uploaded.Location}`)
+        .get("/")
+        .set("Content-Type", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(403);
 
-    await remove(params);
+      await remove(params);
+    } catch (e) {
+      await remove(params);
+      throw e;
+    }
   }, 5000);
 
   it("should create a placeholder object", async () => {
     let queueId = "";
-    await request
-      .get("/storage/queue")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        document: ropstenDocument
-      })
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .expect(res => {
-        queueId = res.body.queueNumber;
-        expect(res.body).toEqual({
-          queueNumber: expect.any(String),
-          key: expect.any(String)
+    try {
+      await request
+        .get("/storage/queue")
+        .set("Content-Type", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect(res => {
+          queueId = res.body.queueNumber;
+          expect(res.body).toEqual({
+            queueNumber: expect.any(String),
+            key: expect.any(String)
+          });
         });
+
+      await request
+        .get(`/storage/get/${queueId}`)
+        .set("Content-Type", "application/json")
+        .expect(400);
+
+      await supertest(`https://${config.bucketName}.s3.amazonaws.com`)
+        .get(`/${queueId}`)
+        .set("Content-Type", "application/json")
+        .expect(403);
+
+      const params = {
+        Bucket: config.bucketName,
+        Key: queueId
+      };
+
+      await remove(params);
+    } catch (e) {
+      await remove({
+        Bucket: config.bucketName,
+        Key: queueId
       });
-
-    await request
-      .get(`/storage/get/${queueId}`)
-      .set("Content-Type", "application/json")
-      .expect(400);
-
-    await supertest(`https://${config.bucketName}.s3.amazonaws.com`)
-      .get(`/${queueId}`)
-      .set("Content-Type", "application/json")
-      .expect(403);
-
-    const params = {
-      Bucket: config.bucketName,
-      Key: queueId
-    };
-
-    await remove(params);
+      throw e;
+    }
   }, 5000);
 });
