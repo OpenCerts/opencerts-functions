0562f866fad427fdbcaecbb6efb973e2df858f0a
Nebulis <nebounet@gmail.com>
2019-12-27 14:34:42 +0800

    feat: migrate to oa-verify v3

--- test/integration/verify.integration.test.js
+++ test/integration/verify.integration.test.js
@@ -1,7 +1,6 @@
 const supertest = require("supertest");
 const ropstenDocument = require("../fixtures/certificate.json");
 const mainnetDocument = require("../fixtures/certificateMainnetValid.json");
-const { unissuedResponse, successfulResponse } = require("../utils/matchers");
 
 const API_ENDPOINT = "http://localhost:3000";
 const request = supertest(API_ENDPOINT);
@@ -21,7 +20,70 @@ describe("verify", () => {
       .expect("Content-Type", /json/)
       .expect(200)
       .expect(res => {
-        expect(res.body).toEqual(successfulResponse({ network: "ropsten" }));
+        expect(res.body).toStrictEqual({
+          data: [
+            {
+              data: true,
+              name: "OpenAttestationHash",
+              status: "VALID",
+              type: "DOCUMENT_INTEGRITY"
+            },
+            {
+              data: {
+                details: [
+                  {
+                    address: "0xc36484efa1544c32ffed2e80a1ea9f0dfc517495",
+                    error:
+                      'contract not deployed (contractAddress="0xc36484efa1544c32ffed2e80a1ea9f0dfc517495", operation="getDeployed", version=4.0.41)',
+                    issued: false
+                  }
+                ],
+                issuedOnAll: false
+              },
+              message: "Certificate has not been issued",
+              name: "OpenAttestationEthereumDocumentStoreIssued",
+              status: "INVALID",
+              type: "DOCUMENT_STATUS"
+            },
+            {
+              message:
+                'Document issuers doesn\'t have "tokenRegistry" property or TOKEN_REGISTRY method',
+              name: "OpenAttestationEthereumTokenRegistryMinted",
+              status: "SKIPPED",
+              type: "DOCUMENT_STATUS"
+            },
+            {
+              data: {
+                details: [
+                  {
+                    address: "0xc36484efa1544c32ffed2e80a1ea9f0dfc517495",
+                    error:
+                      'contract not deployed (contractAddress="0xc36484efa1544c32ffed2e80a1ea9f0dfc517495", operation="getDeployed", version=4.0.41)',
+                    revoked: true
+                  }
+                ],
+                revokedOnAny: true
+              },
+              message: "Certificate has been revoked",
+              name: "OpenAttestationEthereumDocumentStoreRevoked",
+              status: "INVALID",
+              type: "DOCUMENT_STATUS"
+            },
+            {
+              message:
+                'Document issuers doesn\'t have "documentStore" / "tokenRegistry" property or doesn\'t use DNS-TXT type',
+              name: "OpenAttestationDnsTxt",
+              status: "SKIPPED",
+              type: "ISSUER_IDENTITY"
+            }
+          ],
+          summary: {
+            all: false,
+            documentIntegrity: true,
+            documentStatus: false,
+            issuerIdentity: false
+          }
+        });
       });
   });
 
@@ -36,7 +98,64 @@ describe("verify", () => {
       .expect("Content-Type", /json/)
       .expect(200)
       .expect(res => {
-        expect(res.body).toEqual(unissuedResponse({ network: "mainnet" }));
+        expect(res.body).toStrictEqual({
+          data: [
+            {
+              data: true,
+              name: "OpenAttestationHash",
+              status: "VALID",
+              type: "DOCUMENT_INTEGRITY"
+            },
+            {
+              data: {
+                details: [
+                  {
+                    address: "0x007d40224f6562461633ccfbaffd359ebb2fc9ba",
+                    issued: true
+                  }
+                ],
+                issuedOnAll: true
+              },
+              name: "OpenAttestationEthereumDocumentStoreIssued",
+              status: "VALID",
+              type: "DOCUMENT_STATUS"
+            },
+            {
+              message:
+                'Document issuers doesn\'t have "tokenRegistry" property or TOKEN_REGISTRY method',
+              name: "OpenAttestationEthereumTokenRegistryMinted",
+              status: "SKIPPED",
+              type: "DOCUMENT_STATUS"
+            },
+            {
+              data: {
+                details: [
+                  {
+                    address: "0x007d40224f6562461633ccfbaffd359ebb2fc9ba",
+                    revoked: false
+                  }
+                ],
+                revokedOnAny: false
+              },
+              name: "OpenAttestationEthereumDocumentStoreRevoked",
+              status: "VALID",
+              type: "DOCUMENT_STATUS"
+            },
+            {
+              message:
+                'Document issuers doesn\'t have "documentStore" / "tokenRegistry" property or doesn\'t use DNS-TXT type',
+              name: "OpenAttestationDnsTxt",
+              status: "SKIPPED",
+              type: "ISSUER_IDENTITY"
+            }
+          ],
+          summary: {
+            all: false,
+            documentIntegrity: true,
+            documentStatus: true,
+            issuerIdentity: false
+          }
+        });
       });
   });
 });
--- test/utils/matchers.js
+++ test/utils/matchers.js
@@ -1,16 +1,3 @@
-const ROPSTEN_ADDRESS = "0xc36484efa1544c32ffed2e80a1ea9f0dfc517495";
-const MAINNET_ADDRESS = "0x007d40224f6562461633ccfbaffd359ebb2fc9ba";
-
-const getIssuerAddress = network => {
-  switch (network.toLowerCase()) {
-    case "mainnet":
-      return MAINNET_ADDRESS;
-    case "ropsten":
-    default:
-      return ROPSTEN_ADDRESS;
-  }
-};
-
 const uuidV4Regex = new RegExp(
   /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
 );
@@ -34,62 +21,7 @@ const thatIsAQueueNumber = {
   id: expect.stringMatching(uuidV4Regex)
 };
 
-const unissuedResponse = ({ network = "ropsten" }) => {
-  const documentStoreAddress = getIssuerAddress(network);
-  return expect.objectContaining({
-    hash: { checksumMatch: true },
-    issued: {
-      issuedOnAll: false,
-      details: [
-        {
-          address: documentStoreAddress,
-          error: expect.stringMatching("exception"),
-          issued: false
-        }
-      ]
-    },
-    revoked: {
-      revokedOnAny: false,
-      details: [
-        {
-          address: documentStoreAddress,
-          revoked: false
-        }
-      ]
-    },
-    valid: false
-  });
-};
-
-const successfulResponse = ({ network = "ropsten" }) => {
-  const documentStoreAddress = getIssuerAddress(network);
-  return expect.objectContaining({
-    hash: { checksumMatch: true },
-    issued: {
-      issuedOnAll: true,
-      details: [
-        {
-          address: documentStoreAddress,
-          issued: true
-        }
-      ]
-    },
-    revoked: {
-      revokedOnAny: false,
-      details: [
-        {
-          address: documentStoreAddress,
-          revoked: false
-        }
-      ]
-    },
-    valid: true
-  });
-};
-
 module.exports = {
-  successfulResponse,
-  unissuedResponse,
   thatIsRetrievedDocument,
   thatIsUploadResponse,
   thatIsAQueueNumber
