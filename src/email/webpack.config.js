2bec54a79eca52d18fdff0528028ae3b0c2718f7
Nebulis <nebounet@gmail.com>
2020-05-06 17:52:06 +0800

    feat: webpack source map

--- src/email/webpack.config.js
+++ src/email/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const slsw = require("serverless-webpack");
 
 module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  devtool: slsw.lib.webpack.isLocal
+    ? "cheap-module-eval-source-map"
+    : "source-map",
   entry: "./index.js",
   target: "node",
   output: {
--- src/verify/webpack.config.js
+++ src/verify/webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
+const slsw = require("serverless-webpack");
 
 module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  devtool: slsw.lib.webpack.isLocal
+    ? "cheap-module-eval-source-map"
+    : "source-map",
   entry: "./index.js",
   target: "node",
   output: {
