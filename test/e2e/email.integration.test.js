834db6b1a90a69b799e15fa79bb4563c5957218f
RJ <me@rjchow.com>
2020-06-18 14:13:18 +0800

    lint

--- test/e2e/email.integration.test.js
+++ test/e2e/email.integration.test.js
@@ -3,82 +3,98 @@ const supertest = require("supertest");
 const ropstenCertificate = require("../fixtures/certificate.json");
 const mainnetCertificate = require("../fixtures/certificateMainnetValid.json");
 
-const API_ENDPOINT = process.env.EMAIL_ENDPOINT || "https://api-ropsten.opencerts.io/email";
-const API_TIMEOUT = 20000 // api timeout defined in serverless.yml
-
+const API_ENDPOINT =
+  process.env.EMAIL_ENDPOINT || "https://api-ropsten.opencerts.io/email";
+const API_TIMEOUT = 20000; // api timeout defined in serverless.yml
 
 const request = supertest(API_ENDPOINT);
 
 describe("email", () => {
-  it("should works for valid Ropsten certificate", async () => {
-    const apiKey = process.env.EMAIL_INTEGRATION_TEST_API_KEY;
-    await request
-      .post("/")
-      .set("X-API-KEY", apiKey)
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        to: "example@opencerts.io",
-        data: ropstenCertificate
-      })
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .expect(res => {
-        expect(res.body).toEqual({ success: true });
-      });
-  }, API_TIMEOUT);
-
-  it("should fail for invalid certificate (or on wrong network)", async () => {
-    const apiKey = process.env.EMAIL_INTEGRATION_TEST_API_KEY;
-    await request
-      .post("/")
-      .set("X-API-KEY", apiKey)
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        to: "example@opencerts.io",
-        data: mainnetCertificate
-      })
-      .expect("Content-Type", /json/)
-      .expect(400)
-      .expect(res => {
-        expect(res.body).toEqual({ error: "Invalid certificate" });
-      });
-  }, API_TIMEOUT);
-
-  it("should fail if captcha is invalid and missing api key", async () => {
-    await request
-      .post("/")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        to: "example@opencerts.io",
-        data: ropstenCertificate,
-        captcha: "moo"
-      })
-      .expect("Content-Type", /json/)
-      .expect(400)
-      .expect(res => {
-        expect(res.body).toEqual({
-          error: "Invalid captcha or missing API key"
+  it(
+    "should works for valid Ropsten certificate",
+    async () => {
+      const apiKey = process.env.EMAIL_INTEGRATION_TEST_API_KEY;
+      await request
+        .post("/")
+        .set("X-API-KEY", apiKey)
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          to: "example@opencerts.io",
+          data: ropstenCertificate
+        })
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect(res => {
+          expect(res.body).toEqual({ success: true });
         });
-      });
-  }, API_TIMEOUT);
+    },
+    API_TIMEOUT
+  );
 
-  it("should fail if api key is invalid", async () => {
-    await request
-      .post("/")
-      .set("X-API-KEY", "oink")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        to: "example@opencerts.io",
-        data: ropstenCertificate
-      })
-      .expect("Content-Type", /json/)
-      .expect(400)
-      .expect(res => {
-        expect(res.body).toEqual({ error: "Invalid API key" });
-      });
-  }, API_TIMEOUT);
+  it(
+    "should fail for invalid certificate (or on wrong network)",
+    async () => {
+      const apiKey = process.env.EMAIL_INTEGRATION_TEST_API_KEY;
+      await request
+        .post("/")
+        .set("X-API-KEY", apiKey)
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          to: "example@opencerts.io",
+          data: mainnetCertificate
+        })
+        .expect("Content-Type", /json/)
+        .expect(400)
+        .expect(res => {
+          expect(res.body).toEqual({ error: "Invalid certificate" });
+        });
+    },
+    API_TIMEOUT
+  );
+
+  it(
+    "should fail if captcha is invalid and missing api key",
+    async () => {
+      await request
+        .post("/")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          to: "example@opencerts.io",
+          data: ropstenCertificate,
+          captcha: "moo"
+        })
+        .expect("Content-Type", /json/)
+        .expect(400)
+        .expect(res => {
+          expect(res.body).toEqual({
+            error: "Invalid captcha or missing API key"
+          });
+        });
+    },
+    API_TIMEOUT
+  );
+
+  it(
+    "should fail if api key is invalid",
+    async () => {
+      await request
+        .post("/")
+        .set("X-API-KEY", "oink")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          to: "example@opencerts.io",
+          data: ropstenCertificate
+        })
+        .expect("Content-Type", /json/)
+        .expect(400)
+        .expect(res => {
+          expect(res.body).toEqual({ error: "Invalid API key" });
+        });
+    },
+    API_TIMEOUT
+  );
 });
--- test/e2e/storage.integration.test.js
+++ test/e2e/storage.integration.test.js
@@ -7,70 +7,83 @@ const {
   thatIsAQueueNumber
 } = require("../utils/matchers");
 
-const API_ENDPOINT = process.env.STORAGE_ENDPOINT || "https://api-ropsten.opencerts.io/storage";
-const API_TIMEOUT = 30000 // api timeout defined in serverless.yml
+const API_ENDPOINT =
+  process.env.STORAGE_ENDPOINT || "https://api-ropsten.opencerts.io/storage";
+const API_TIMEOUT = 30000; // api timeout defined in serverless.yml
 
 const request = supertest(API_ENDPOINT);
 
 describe("storage endpoint test", () => {
-  test("should create a new document when no placeholder object is there", async () => {
-    await request
-      .post("/")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        document: ropstenDocument
-      })
-      .expect(res => {
-        expect(res.body).toEqual(thatIsUploadResponse);
-      });
-  }, API_TIMEOUT);
+  test(
+    "should create a new document when no placeholder object is there",
+    async () => {
+      await request
+        .post("/")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          document: ropstenDocument
+        })
+        .expect(res => {
+          expect(res.body).toEqual(thatIsUploadResponse);
+        });
+    },
+    API_TIMEOUT
+  );
 
-  test("should fail when you try to create at a path that hasn't been initialised", async () => {
-    await request
-      .post("/foo")
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        document: ropstenDocument
-      })
-      .expect(400);
-  }, API_TIMEOUT);
+  test(
+    "should fail when you try to create at a path that hasn't been initialised",
+    async () => {
+      await request
+        .post("/foo")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          document: ropstenDocument
+        })
+        .expect(400);
+    },
+    API_TIMEOUT
+  );
 
-  test("should retrieve the document created", async () => {
-    let documentKey;
-    await request
-      .get("/queue")
-      .set("Content-Type", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .expect(res => {
-        documentKey = res.body.id;
-        expect(res.body).toEqual(thatIsAQueueNumber);
-      });
+  test(
+    "should retrieve the document created",
+    async () => {
+      let documentKey;
+      await request
+        .get("/queue")
+        .set("Content-Type", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect(res => {
+          documentKey = res.body.id;
+          expect(res.body).toEqual(thatIsAQueueNumber);
+        });
 
-    await request
-      .post(`/${documentKey}`)
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .send({
-        document: ropstenDocument,
-        id: documentKey
-      })
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .expect(res => {
-        expect(res.body.id).toEqual(documentKey);
-        expect(res.body).toEqual(thatIsUploadResponse);
-      });
+      await request
+        .post(`/${documentKey}`)
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .send({
+          document: ropstenDocument,
+          id: documentKey
+        })
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect(res => {
+          expect(res.body.id).toEqual(documentKey);
+          expect(res.body).toEqual(thatIsUploadResponse);
+        });
 
-    await request
-      .get(`/${documentKey}`)
-      .set("Content-Type", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .expect(res => {
-        expect(res.body).toEqual(thatIsRetrievedDocument);
-      });
-  }, API_TIMEOUT);
+      await request
+        .get(`/${documentKey}`)
+        .set("Content-Type", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect(res => {
+          expect(res.body).toEqual(thatIsRetrievedDocument);
+        });
+    },
+    API_TIMEOUT
+  );
 });
